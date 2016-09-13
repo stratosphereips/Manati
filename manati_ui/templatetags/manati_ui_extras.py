48cdb9f3e9ed5c52b1f3b733d55bdfd821cea365
eldraco <eldraco@gmail.com>
2016-09-13 10:45:16 +0200

    Delete the call to git, so it can be used better if git is not installed

--- manati_ui/templatetags/manati_ui_extras.py
+++ manati_ui/templatetags/manati_ui_extras.py
@@ -4,9 +4,9 @@ import subprocess
 register = template.Library()
 
 
-@register.simple_tag
-def get_git_revision_number():
-    return subprocess.check_output(['git', 'rev-list', '--count', 'HEAD'])
+#@register.simple_tag
+#def get_git_revision_number():
+#    return subprocess.check_output(['git', 'rev-list', '--count', 'HEAD'])
 
 @register.simple_tag
 def display_flash_messages(messages):
