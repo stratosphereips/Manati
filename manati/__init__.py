1bfdf721b459a84b1be17b266e7463743e947b52
Raúl Benitez Netto <raulbeni@gmail.com>
2016-10-12 21:16:07 +0200

    adding version with the feedback changes

--- manati/__init__.py
+++ manati/__init__.py
@@ -3,7 +3,7 @@ ManaTI Project
 """
 
 __title__ = 'ManaTI Project'
-__version__ = '0.5'
+__version__ = '0.5.1'
 __author__ = 'Raul Carmelo Benitez Netto'
 __copyright__ = 'Copyright 2016-2017 CISCO - CTU - FEL'
 
