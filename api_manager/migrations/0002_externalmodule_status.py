f6f6617e9299eea94fa4b892c5bec0763e935c11
Raúl Benitez Netto <raulbeni@gmail.com>
2016-10-28 12:54:54 +0200

    when you run the migrate of api_manager, the migrations of the background task will be run too

--- api_manager/migrations/0002_externalmodule_status.py
+++ api_manager/migrations/0002_externalmodule_status.py
@@ -3,7 +3,12 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.core import management
 
+
+def run_migrations_background_task(apps, schema_editor):
+    management.call_command("makemigrations", "background_task")
+    management.call_command("migrate", "background_task")
 
 class Migration(migrations.Migration):
 
@@ -17,4 +22,5 @@ class Migration(migrations.Migration):
             name='status',
             field=models.CharField(choices=[('idle', 'idle'), ('running', 'running'), ('removed', 'removed')], default='idle', max_length=20),
         ),
+        migrations.RunPython(run_migrations_background_task),
     ]
