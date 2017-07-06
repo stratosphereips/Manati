b22f2711269f45fbd1f761cb0cd054e82378edaa
Raúl Benitez Netto <raulbeni@gmail.com>
2017-07-06 19:45:56 +0200

    minor fix

--- manati/settings.py
+++ manati/settings.py
@@ -260,30 +260,28 @@ if os.environ.get('DJANGO_DEBUG'):
 GUARDIAN_GET_INIT_ANONYMOUS_USER = 'manati_ui.models.get_anonymous_user_instance'
 
 if DEBUG:
-   # INTERNAL_IPS = ('127.0.0.1', 'localhost',)
-   MIDDLEWARE_CLASSES += [
-       'debug_toolbar.middleware.DebugToolbarMiddleware',
-   ]
+    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+    MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
 
-   INSTALLED_APPS += [
-       'debug_toolbar',
-   ]
+    INSTALLED_APPS += [
+           'debug_toolbar',
+    ]
 
-   DEBUG_TOOLBAR_PANELS = [
-       'debug_toolbar.panels.versions.VersionsPanel',
-       'debug_toolbar.panels.timer.TimerPanel',
-       'debug_toolbar.panels.settings.SettingsPanel',
-       'debug_toolbar.panels.headers.HeadersPanel',
-       'debug_toolbar.panels.request.RequestPanel',
-       'debug_toolbar.panels.sql.SQLPanel',
-       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-       'debug_toolbar.panels.templates.TemplatesPanel',
-       'debug_toolbar.panels.cache.CachePanel',
-       'debug_toolbar.panels.signals.SignalsPanel',
-       'debug_toolbar.panels.logging.LoggingPanel',
-       'debug_toolbar.panels.redirects.RedirectsPanel',
-   ]
+    DEBUG_TOOLBAR_PANELS = [
+           'debug_toolbar.panels.versions.VersionsPanel',
+           'debug_toolbar.panels.timer.TimerPanel',
+           'debug_toolbar.panels.settings.SettingsPanel',
+           'debug_toolbar.panels.headers.HeadersPanel',
+           'debug_toolbar.panels.request.RequestPanel',
+           'debug_toolbar.panels.sql.SQLPanel',
+           'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+           'debug_toolbar.panels.templates.TemplatesPanel',
+           'debug_toolbar.panels.cache.CachePanel',
+           'debug_toolbar.panels.signals.SignalsPanel',
+           'debug_toolbar.panels.logging.LoggingPanel',
+           'debug_toolbar.panels.redirects.RedirectsPanel',
+       ]
 
-   DEBUG_TOOLBAR_CONFIG = {
-       'INTERCEPT_REDIRECTS': False,
-   }
+    DEBUG_TOOLBAR_CONFIG = {
+           'INTERCEPT_REDIRECTS': False,
+       }
