9f13922a03f87cda10523e3e1749a4cb61d08315
Raúl Benítez Netto <raulbeni@gmail.com>
2017-08-09 18:09:49 +0200

    minor change

--- manati/settings.py
+++ manati/settings.py
@@ -103,24 +103,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'manati_db',  # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'manati_db_user',
-            'PASSWORD': 'password',
-            'HOST': 'localhost',
-            # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '5432',  # Set to empty string for default.
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default=config('DATABASE_URL'))
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 
 
