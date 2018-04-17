b67b942e491dfe4a1c6225adba87bcfcbe81a893
Raúl Benítez Netto <raulbeni@gmail.com>
2018-04-17 21:26:15 +0200

    * adding logic to parse netflows [uninetflow, binetflow] * refactoring JS logic to use ES6, spliting the code in several files to a better understanding * fixing bug in analysis session model  validation

---
+++ manati/analysis_sessions/static/manati_ui/js/modules/controllers/shortcuts.js
@@ -0,0 +1,251 @@
+import {scrollIntoViewIfNeeded} from '../helpers/utils.js';
+
+class Shortcuts {
+    constructor(analysis_session_obj) {
+        this.analysis_session_obj = analysis_session_obj;
+        this.dynamic_table_obj = analysis_session_obj.dynamic_table_obj;
+        this.modals = analysis_session_obj.modals;
+        // this._dt = analysis_session_obj.dynamic_table_obj.dt;
+
+    }
+
+    define_hotkeys(){
+        let thiz = this;
+        let preventDefault = function (e) {
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                // internet explorer
+                e.returnValue = false;
+            }
+        };
+        // active sync button
+        Mousetrap.bind(['ctrl+s', 'command+s'], function (e) {
+            preventDefault(e);
+            if (thiz.isSaved()) this.analysis_session_obj.syncDB(true);
+        });
+        // mark malicious
+        Mousetrap.bind(['ctrl+m', 'command+m'], function (e) {
+            preventDefault(e);
+            this.dynamic_table_obj.labelingRows('malicious');
+        });
+        // mark legitimate
+        Mousetrap.bind(['ctrl+l', 'command+l'], function (e) {
+            preventDefault(e);
+            this.dynamic_table_obj.labelingRows('legitimate');
+        });
+        // mark suspicious
+        Mousetrap.bind(['ctrl+i', 'command+i'], function (e) {
+            preventDefault(e);
+            this.dynamic_table_obj.labelingRows('suspicious');
+        });
+        // mark false positive
+        Mousetrap.bind(['ctrl+p', 'command+p'], function (e) {
+            preventDefault(e);
+            this.dynamic_table_obj.labelingRows('falsepositive');
+        });
+        // mark undefined
+        Mousetrap.bind(['ctrl+u', 'command+u'], function (e) {
+            preventDefault(e);
+            this.dynamic_table_obj.labelingRows('undefined');
+        });
+        // unselect selected rows
+        Mousetrap.bind(['shift+ctrl+u', 'shift+command+u'], function (e) {
+            preventDefault(e);
+            $('tr.selected').removeClass('selected');
+        });
+        // Filter all Malicious
+        Mousetrap.bind(['ctrl+1', 'command+1'], function (e) {
+            preventDefault(e);
+            thiz.analysis_session_obj.executeFilterBtn('malicious');
+        });
+        // Filter all Legitimate
+        Mousetrap.bind(['ctrl+2', 'command+2'], function (e) {
+            preventDefault(e);
+            thiz.analysis_session_obj.executeFilterBtn('legitimate');
+        });
+        // Filter all Suspicious
+        Mousetrap.bind(['ctrl+3', 'command+3'], function (e) {
+            preventDefault(e);
+            thiz.analysis_session_obj.executeFilterBtn('suspicious');
+        });
+        // Filter all False Positive
+        Mousetrap.bind(['ctrl+4', 'command+4'], function (e) {
+            preventDefault(e);
+            thiz.analysis_session_obj.executeFilterBtn('falsepositive');
+        });
+        // Filter all Undefined
+        Mousetrap.bind(['ctrl+5', 'command+5'], function (e) {
+            preventDefault(e);
+            thiz.analysis_session_obj.executeFilterBtn('undefined');
+        });
+        //  // Unfilter everthing
+        // Mousetrap.bind(['ctrl+5', 'command+0'], function(e) {
+        //     preventDefault(e);
+        //     // TO-DO
+        // });
+        //
+        //  // Unfilter everthing
+        // Mousetrap.bind(['space', 'space'], function(e) {
+        //     preventDefault(e);
+        //     // TO-DO
+        // });
+        // open VirusTotal Modal By domain, the first selected weblog
+        Mousetrap.bind(['ctrl+shift+v', 'command+shift+v'], function (e) {
+            preventDefault(e);
+            let qn = thiz.dynamic_table_obj.get_url_data_by_class('.action');
+            thiz.modals.consultVirusTotal(qn, "domain");
+        });
+        // open WHOIS Modal By domain, the first selected weblog
+        Mousetrap.bind(['ctrl+shift+p', 'command+shift+p'], function (e) {
+            preventDefault(e);
+            let qn = thiz.dynamic_table_obj.get_url_data_by_class('.action');
+            thiz.modals.consultWhois(qn, "domain");
+        });
+        // open VirusTotal Modal By IP, the first selected weblog
+        Mousetrap.bind(['ctrl+shift+i', 'command+shift+i'], function (e) {
+            preventDefault(e);
+            let qn = thiz.dynamic_table_obj.get_dist_ip_data_by_class('.action');
+            thiz.modals.consultVirusTotal(qn, "ip");
+        });
+        // open WHOIS Modal By IP, the first selected weblog
+        Mousetrap.bind(['ctrl+shift+o', 'command+shift+o'], function (e) {
+            // preventDefault(e);
+            // let verdict = thiz.dynamic_table_obj.get_verdict_data_by_class('.action');
+            // thiz.dynamic_table_obj.setBulkVerdict_WORKER(verdict, _bulk_marks_wbs[CLASS_MC_END_POINTS_SERVER_STR]);
+        });
+        //show whois similarity modal
+        Mousetrap.bind(['ctrl+shift+d', 'command+shift+d'], function (e) {
+            preventDefault(e);
+            let weblog_id = thiz.dynamic_table_obj.get_dt_id_data_by_class('.action');
+            weblog_id = weblog_id.split(":").length <= 1 ? thiz.getAnalysisSessionId() + ":" + weblog_id : weblog_id;
+            this.modals.getWeblogsWhoisRelated(weblog_id);
+        });
+
+        // VI-Style
+        // moving down with J
+        Mousetrap.bind(['j'], function (e) {
+            preventDefault(e);
+            let current_tr = $('#weblogs-datatable tbody tr.action').first();
+            let next_tr;
+            if (current_tr.length) {
+                next_tr = current_tr.next().first();
+                if (!next_tr.length) {
+                    //move the page if it is possible
+                    let current_page = this._dt.page.info().page;
+                    if (this._dt.page.info().pages > current_page + 1) {
+                        // moving to the next one
+                        this._dt.page(current_page + 1).draw('page');
+                    } else {
+                        // moving to the first page, first row
+                        this._dt.page(0).draw('page');
+                    }
+                    next_tr = $('#weblogs-datatable tbody tr').first();
+                    current_tr = null;
+                }
+            } else {
+                next_tr = $('#weblogs-datatable tbody tr').first();
+            }
+
+            $('#weblogs-datatable tbody tr.action').removeClass('action');
+            next_tr.addClass('action');
+            if (current_tr) {
+                scrollIntoViewIfNeeded(current_tr[0])
+            } else {
+                $("html, body").animate({scrollTop: 0}, "slow");
+            }
+
+        });
+
+        // moving up with k
+        Mousetrap.bind(['k'], function (e) {
+            preventDefault(e);
+            let scroll_tr;
+            let current_tr = $('#weblogs-datatable tbody tr.action').first();
+            let prev_tr;
+            if (current_tr.length) {
+                prev_tr = current_tr.prev().first();
+                if (!prev_tr.length) {
+                    //move the page if it is possible
+                    let current_page = this._dt.page.info().page;
+                    if (0 <= current_page - 1) {
+                        // moving to the previous page
+                        this._dt.page(current_page - 1).draw('page');
+                    } else {
+                        // moving to the last page, last row
+                        let pages = this._dt.page.info().pages;
+                        this._dt.page(pages - 1).draw('page');
+                    }
+                    prev_tr = $('#weblogs-datatable tbody tr').last();
+                    scroll_tr = prev_tr;
+                    current_tr = null;
+                }
+            } else {
+                prev_tr = $('#weblogs-datatable tbody tr').last();
+            }
+
+            $('#weblogs-datatable tbody tr.action').removeClass('action');
+            prev_tr.addClass('action');
+            scroll_tr = scroll_tr ? scroll_tr : prev_tr.prev();
+            if (scroll_tr.length) {
+                scrollIntoViewIfNeeded(scroll_tr[0]);
+            } else {
+                $("html, body").animate({scrollTop: 0}, "slow");
+            }
+
+        });
+        // select row to be label.
+        Mousetrap.bind(['space'], function (e) {
+            preventDefault(e);
+            let current_tr = $('#weblogs-datatable tbody tr.action').first();
+            current_tr.toggleClass('selected');
+
+        });
+
+        Mousetrap.bind(['left'], function (e) {
+            preventDefault(e);
+            let pages = this._dt.page.info().pages;
+            let current_page = this._dt.page.info().page;
+            if (current_page - 1 >= 0) {
+                this._dt.page(current_page - 1).draw('page');
+            } else {
+                this._dt.page(pages - 1).draw('page');
+            }
+        });
+
+        Mousetrap.bind(['right'], function (e) {
+            preventDefault(e);
+            let pages = this._dt.page.info().pages;
+            let current_page = this._dt.page.info().page;
+            if (current_page + 1 < pages) {
+                thiz.dynamic_table_obj.dt.page(current_page + 1).draw('page');
+            } else {
+                thiz.dynamic_table_obj.dt.page(0).draw('page');
+            }
+        });
+
+        //mark all the weblogs in the current session with the same IP
+        Mousetrap.bind(['p'], function (e) {
+            preventDefault(e);
+            let ip_value = thiz.dynamic_table_obj.get_dist_ip_data_by_class('.action');
+            let verdict = thiz.dynamic_table_obj.get_verdict_data_by_class('.action');
+            thiz.dynamic_table_obj.setBulkVerdict_WORKER(verdict, _helper.getFlowsGroupedBy(COL_END_POINTS_SERVER_STR, ip_value));
+        });
+
+        //mark all  the weblogs in the current session with the same domain
+        Mousetrap.bind(['d'], function (e) {
+            preventDefault(e);
+            let url = thiz.dynamic_table_obj.dt.rows('.action').data()[0][COLUMN_HTTP_URL].toString();
+            let domain = findDomainOfURL(url); // getting domain
+            let verdict = thiz.dynamic_table_obj.dt.rows('.action').data()[0][COLUMN_VERDICT].toString();
+            thiz.dynamic_table_obj.setBulkVerdict_WORKER(verdict, _helper.getFlowsGroupedBy(COL_HTTP_URL_STR, domain));
+        });
+
+
+    };
+
+
+}
+
+export default Shortcuts;
+
--- manati/analysis_sessions/static/manati_ui/js/struct_helper.js
+++ manati/analysis_sessions/static/manati_ui/js/struct_helper.js
@@ -8,7 +8,7 @@ function FlowsProcessed(col_host_str,co_ip_str){
     var COL_IP_STR = co_ip_str;
     var _int_flows_grouped = {};
     var REG_EXP_DOMAINS = /[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
-    var REG_EXP_IP = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
+    var REG_EXP_IP = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
     function groupingFlow(key_flow, key_group, value){
         if(key_flow === null || key_group === null || value === null) return false;
         if(!(_int_flows_grouped[key_flow] instanceof Object))_int_flows_grouped[key_flow] = {};
@@ -21,8 +21,8 @@ function FlowsProcessed(col_host_str,co_ip_str){
     function findDomainOfURL(url){
         if (typeof url !== "string") return null;
         var matching_domain = null;
-        var domain = ( (matching_domain = url.match(REG_EXP_DOMAINS)) != null )|| matching_domain != undefined && matching_domain.length > 0 ? matching_domain[0] : null ;
-        domain = (domain == null)  && ((matching_domain = url.match(REG_EXP_IP)) != null) || matching_domain != undefined && matching_domain.length > 0 ? matching_domain[0] : null;
+        var domain = ( (matching_domain = url.match(REG_EXP_IP)) != null )|| matching_domain != undefined && matching_domain.length > 0 ? matching_domain[0] : null ;
+        domain = (domain == null)  && ((matching_domain = url.match(REG_EXP_DOMAINS)) != null) || matching_domain != undefined && matching_domain.length > 0 ? matching_domain[0] : null;
         return domain
     }
     function findIP(url){
@@ -51,7 +51,7 @@ function FlowsProcessed(col_host_str,co_ip_str){
     this.addFlows = function(flow){
         _.each(flow, function (v,k){
             if(k === COL_HOST_STR || k === COL_IP_STR){
-                var d;
+                let d;
                 if((d = findDomainOfURL(v)) !== null){
                     groupingFlow(k,d,flow);
                 }
@@ -62,7 +62,9 @@ function FlowsProcessed(col_host_str,co_ip_str){
 
     };
     this.addBulkFlows = function (flows){
-        for(var i = 0; i< flows.length; i++) {
+        if (thiz.COL_HOST_STR == null && thiz.COL_IP_STR == null) return;
+
+        for(let i = 0; i< flows.length; i++) {
             thiz.addFlows(flows[i]);
         }
     };
@@ -71,52 +73,52 @@ function FlowsProcessed(col_host_str,co_ip_str){
         if(flows_grouped!==undefined){
             _int_flows_grouped=flows_grouped;
         }
-        var table = document.createElement('table');
+        let table = document.createElement('table');
         table.setAttribute("id", "statistics_table");
         table.classList = ["table"];
-        var thead = document.createElement('thead');
-        var tr = document.createElement('tr');
-        var th1 = document.createElement('th');
-        var th2 = document.createElement('th');
-        var th3 = document.createElement('th');
-        var text_h1 = document.createTextNode('Key Group');
-        var text_h2 = document.createTextNode('Key Flow');
-        var text_h3 = document.createTextNode('Amount');
+        let thead = document.createElement('thead');
+        let tr = document.createElement('tr');
+        let th1 = document.createElement('th');
+        let th2 = document.createElement('th');
+        let th3 = document.createElement('th');
+        let text_h1 = document.createTextNode('Key Group');
+        let text_h2 = document.createTextNode('Key Flow');
+        let text_h3 = document.createTextNode('Amount');
         th1.appendChild(text_h1);
         th2.appendChild(text_h2); th3.appendChild(text_h3);
         tr.appendChild(th2); tr.appendChild(th3);
         tr.appendChild(th1);
         thead.appendChild(tr);
         table.appendChild(thead);
-        var tbody = document.createElement('tbody');
+        let tbody = document.createElement('tbody');
         $.each(_.keys(_int_flows_grouped),function (index, key_group) {
-            // var tr = document.createElement('tr');
-            var key_flows = _.keys(_int_flows_grouped[key_group]);
-            // var td1 = document.createElement('td');
-            // var text = document.createTextNode(key_group);
+            // let tr = document.createElement('tr');
+            let key_flows = _.keys(_int_flows_grouped[key_group]);
+            // let td1 = document.createElement('td');
+            // let text = document.createTextNode(key_group);
             // td1.rowSpan = key_flows.length;
             // td1.appendChild(text);
             // tr.appendChild(td1);
             $.each(key_flows, function (i,key_flow) {
-                var tr = document.createElement('tr');
-                var td1 = document.createElement('td');
-                var text = document.createTextNode(key_group);
+                let tr = document.createElement('tr');
+                let td1 = document.createElement('td');
+                let text = document.createTextNode(key_group);
                 td1.appendChild(text);
 
                 // if(tr==null) {
                 //     tr=document.createElement('tr');
-                //     // var td1 = document.createElement('td');
-                //     // var text = document.createTextNode(key_group);
+                //     // let td1 = document.createElement('td');
+                //     // let text = document.createTextNode(key_group);
                 //     // td1.appendChild(text);
                 //     // tr.appendChild(td1);
                 // }
-                var td2 = document.createElement('td');
-                var text1 = document.createTextNode(key_flow);
+                let td2 = document.createElement('td');
+                let text1 = document.createTextNode(key_flow);
                 td2.appendChild(text1);
                 tr.appendChild(td2);
-                var size = thiz.getFlowsGroupedBy(key_group, key_flow).length;
-                var td3 = document.createElement('td');
-                var text3 = document.createTextNode(size.toString());
+                let size = thiz.getFlowsGroupedBy(key_group, key_flow).length;
+                let td3 = document.createElement('td');
+                let text3 = document.createTextNode(size.toString());
                 td3.appendChild(text3);
                 tr.appendChild(td3);
                 tr.appendChild(td1);
